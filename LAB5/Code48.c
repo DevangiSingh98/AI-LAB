6eaac761cab13bc1f706843639a61ba04592158c
Devangi Singh <singhdevangi@outlook.com>
2024-09-25 21:27:28 +0530

    Add files via upload

---
+++ LAB5/Code48.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n;          //decalration
+    
+    //User input
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for (i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (j = i; j < n; j++) {
+            printf(" ");
+        }
+        // Print numbers
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        // Move to the next line
+        printf("\n");
+    }
+
+    return 0;
+}
+
